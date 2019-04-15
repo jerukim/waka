b3405bcee5ab986b65fbf2b47932d38bcada03c0
jerumarkable <mikurej95@gmail.com>
2019-04-15 15:20:54 -0500

    initial commit

---
+++ src/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => (
+  <header>
+    <div className="container">
+      <h1>ANDREW</h1>
+      <a> test </a>
+    </div>
+  </header>
+);
+
+export default Navbar;
