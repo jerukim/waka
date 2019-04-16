a1f67693fbf251bc496181768d8485eb2eefa2ae
jerumarkable <mikurej95@gmail.com>
2019-04-16 18:18:59 -0500

    changed all class components to functional components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './App.css';
 
 import Navbar from './navbar/navbar';
 import Routes from './routes';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Routes />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes />
+    </div>
+  );
+};
 
 export default App;
